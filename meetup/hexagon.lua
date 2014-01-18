23456efb25b589e70ba24d0c61d3b65c837ee1df
Jason Mayer <jason.mayer@gmail.com>
2014-01-17 22:52:37 -0600

    initial commit

---
+++ meetup/hexagon.lua
@@ -0,0 +1,51 @@
+local Hexagon = {}
+Hexagon.__index = Hexagon -- failed table lookups on the instances should fallback to the class table, to get methods
+
+local generate_points = function(self)
+    local h = {}
+    h.l  = (self.height / 2) / math.sin(45)
+    h.dX = math.cos(45) * h.l
+    h.dY = math.sin(45) * h.l
+    h.cX = h.dX + (h.l / 2)
+    h.cY = h.dY
+    self.vertices = {
+        0,                 h.dY,
+        h.dX,              0,
+        h.dX + h.l,        0,
+        h.dX + h.l + h.dX, h.dY,
+        h.dX + h.l,        h.dY + h.dY,
+        h.dX,              h.dY + h.dY
+    }
+end
+Hexagon.generate_points = generate_points
+
+local touch = function(self, event)
+    if event.phase == "began" then
+        print("huh?", self.x)
+        self.displayObject.strokeWidth = 10
+        self.displayObject:setStrokeColor( 0, 1, 1 )
+    elseif event.phase == "ended" then
+        self.displayObject.strokeWidth = 0
+    end
+end
+
+local new = function(x, y, height)
+    local self = setmetatable({}, Hexagon)
+    self.x = x
+    self.y = y
+    
+    self.height = height
+    self:generate_points()
+    self.touch = touch
+    
+    return self
+end
+Hexagon.new = new
+
+local draw = function(self)
+    self.displayObject = display.newPolygon( self.height * self.x, self.height * self.y, self.vertices )
+    self.displayObject:addEventListener( "touch", self)
+end
+Hexagon.draw = draw
+
+return Hexagon
---
+++ meetup/level1.lua
@@ -0,0 +1,84 @@
+-----------------------------------------------------------------------------------------
+--
+-- level1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local storyboard = require( "storyboard" )
+local scene = storyboard.newScene()
+local Hexagon = require("hexagon")
+
+-- include Corona's "physics" library
+local physics = require "physics"
+physics.start(); physics.pause()
+
+--------------------------------------------
+
+-- forward declarations and other locals
+local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+
+-- Called when the scene's view does not exist:
+
+function scene:createScene( event )
+	local group = self.view
+
+	-- create a grey rectangle as the backdrop
+	local background = display.newRect( 0, 0, screenW, screenH )
+	background.anchorX = 0
+	background.anchorY = 0
+	background:setFillColor( .5 )
+	
+	
+	group:insert( background )
+    local hex = Hexagon.new(1, 1, 50)
+    hex:draw()
+    local hex2 = Hexagon.new(2, 2, 50)
+    hex2:draw()
+    
+end
+
+-- Called immediately after scene has moved onscreen:
+function scene:enterScene( event )
+	local group = self.view
+	
+	physics.start()
+	
+end
+
+-- Called when scene is about to move offscreen:
+function scene:exitScene( event )
+	local group = self.view
+	
+	physics.stop()
+	
+end
+
+-- If scene's view is removed, scene:destroyScene() will be called just prior to:
+function scene:destroyScene( event )
+	local group = self.view
+	
+	package.loaded[physics] = nil
+	physics = nil
+end
+
+-----------------------------------------------------------------------------------------
+-- END OF YOUR IMPLEMENTATION
+-----------------------------------------------------------------------------------------
+
+-- "createScene" event is dispatched if scene's view does not exist
+scene:addEventListener( "createScene", scene )
+
+-- "enterScene" event is dispatched whenever scene transition has finished
+scene:addEventListener( "enterScene", scene )
+
+-- "exitScene" event is dispatched whenever before next scene's transition begins
+scene:addEventListener( "exitScene", scene )
+
+-- "destroyScene" event is dispatched before view is unloaded, which can be
+-- automatically unloaded in low memory situations, or explicitly via a call to
+-- storyboard.purgeScene() or storyboard.removeScene().
+scene:addEventListener( "destroyScene", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
